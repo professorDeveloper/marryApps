8dd8829f578ec5fb74b8a2c13e8745de1127281e
Muslimbek Yarashev <108422258+cullinanMy@users.noreply.github.com>
2025-12-17 14:20:18 +0500

    update

--- app/internal/api/docs/docs.go
+++ app/internal/api/docs/docs.go
@@ -1245,9 +1245,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "back.blitz.yurtal.tech",
 	BasePath:         "/",
-	Schemes:          []string{"http"},
+	Schemes:          []string{"https"},
 	Title:            "Blitz API",
 	Description:      "Blitz API server with multi-language support (de, uz)",
 	InfoInstanceName: "swagger",
