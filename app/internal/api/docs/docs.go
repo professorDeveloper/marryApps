86c932df4c7a9d1588a1ed683873b32758cc7b20
JavohKhasanov <javoxkhasanov@gmail.com>
2026-01-24 11:42:57 +0500

    swagger chnaged

--- app/internal/api/docs/docs.go
+++ app/internal/api/docs/docs.go
@@ -17507,7 +17507,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "",
+	Host:             "back.maryai.yurtal.tech",
 	BasePath:         "/",
 	Schemes:          []string{"https"},
 	Title:            "MaryAI API",
