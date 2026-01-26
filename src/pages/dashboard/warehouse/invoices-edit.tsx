0933e46a664edbfaf0b5318748e30b8ccd354a76
Hamdam Hurmatullayev <hurmatullayevhamdambek@gmail.com>
2026-01-26 16:38:29 +0500

    fix bugs

--- src/pages/dashboard/warehouse/invoices-edit.tsx
+++ src/pages/dashboard/warehouse/invoices-edit.tsx
@@ -1,6 +1,6 @@
 import { CONFIG } from 'src/global-config';
 
-import { InvoicesEditView } from 'src/sections/warehouse/supplier-edit-view';
+import { SupplierEditView } from 'src/sections/warehouse/supplier-edit-view';
 
 // ============================================================================
 
@@ -11,7 +11,7 @@ export default function Page() {
         <>
             <title>{metadata.title}</title>
 
-            <InvoicesEditView />
+            <SupplierEditView />
         </>
     );
 }
