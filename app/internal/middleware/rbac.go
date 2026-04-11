34842ecfca187d8945a5fc5644efd5d747f77946
bahodir <isomiddinovbahodir7@gmail.com>
2026-04-11 16:37:09 +0500

    fix perms n2

--- app/internal/middleware/rbac.go
+++ app/internal/middleware/rbac.go
@@ -81,11 +81,4 @@ var (
 		model.RoleManager,
 		model.RoleSuperAdmin,
 	}
-	RolesCanGetPrinterSettings = []string{
-		model.RoleWaiter,
-		model.RoleCashier,
-		model.RoleAdmin,
-		model.RoleManager,
-		model.RoleSuperAdmin,
-	}
 )
