35ee19dcded593efa798e21003c531efc57cea18
javokhir <javokhir.hayitmirzayev@gmail.com>
2026-04-23 23:28:47 +0500

    Remove MinConns to prevent connection exhaustion in production

--- app/pkg/postgres/postgres.go
+++ app/pkg/postgres/postgres.go
@@ -52,7 +52,6 @@ func tryToConnectWithAttempts(ctx context.Context, maxAttempts int, maxPoolSize 
 		log.Fatalf("Unable to parse config: %v\n", err)
 	}
 	poolConfig.MaxConns = maxPoolSize
-	poolConfig.MinConns = 2
 	poolConfig.MaxConnIdleTime = 5 * time.Minute
 	poolConfig.MaxConnLifetime = 30 * time.Minute
 	poolConfig.HealthCheckPeriod = 1 * time.Minute
