ccd425b451d31b7f5602d05e84955f6a9bfb5a67
javokhir <javokhir.hayitmirzayev@gmail.com>
2026-06-14 01:15:44 +0500

    Fix import order in invoice-details-standalone-list-view

--- src/sections/warehouse/invoice/invoice-details-standalone-list-view.tsx
+++ src/sections/warehouse/invoice/invoice-details-standalone-list-view.tsx
@@ -41,8 +41,8 @@ import { DashboardContent } from 'src/layouts/dashboard';
 import { Iconify } from 'src/components/iconify';
 import { GenericViewModal } from 'src/components/generic-view-view';
 
-import { CELL_SX } from 'src/sections/common/data-table/utils/constants';
 import { DataTable } from 'src/sections/common/data-table';
+import { CELL_SX } from 'src/sections/common/data-table/utils/constants';
 
 import { MetadataEntity } from 'src/types/metadata';
 
